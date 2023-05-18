ad2d6edc146cee7ed7cd07a033c1a4fb36efa7c3
kalen510 <kalen.wiley@gmail.com>
2023-05-18 17:18:29 -0400

    basic footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-full p-4 fixed bottom-0 flex justify-around">
+      <p>LinkedIn</p>
+      <p>GitHub</p>
+      <p>Email</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import Nav from '../Nav';
 
 function Header() {
   return (
-    <header className="px-5 py-5 md:flex md:justify-between">
+    <header className="w-full p-5 md:flex md:justify-between">
       <h2><a href="/" className="text-2xl">Kalen Wiley</a></h2>
       <Nav></Nav>
     </header>
