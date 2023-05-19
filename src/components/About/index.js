25681297018533fd92ae727605d6c4c275145625
kalen510 <kalen.wiley@gmail.com>
2023-05-18 21:09:12 -0400

    add avatar

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 function About() {
   return (
     <section>
-      <h1 className="text-3xl">About Me</h1>
+      <h1 className="text-3xl">Full-Stack Web Developer</h1>
       <div>
-        
+        <img src={require('../../assets/images/Memoji.png')} alt="Memoji character of Kalen" className="" />
       </div>
     </section>
   )
