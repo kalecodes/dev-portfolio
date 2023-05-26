641c9f1f733badf737af58f00b6be1d2e3890244
kalen510 <kalen.wiley@gmail.com>
2023-05-25 22:28:23 -0400

    responsive resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,12 +4,12 @@ import React from 'react';
 function Resume() {
   return (
     <section id="resume" className="flex justify-center">
-      <div className="w-4/5">
-        <div className="flex justify-between py-10 px-10">
-          <div className="text-5xl text-left">
+      <div className="md:w-4/5">
+        <div className="flex flex-col md:flex-row justify-between py-10 px-10">
+          <div className="text-5xl md:text-left">
             Resume
           </div>
-          <a href="https://drive.google.com/file/d/1IWfyqQS1xVeOQIKEF1uXWiaJKkua_Ut9/view?usp=sharing" target="_blank" rel='noreferrer'><button className="p-3 rounded-lg bg-sky-950 text-white">View in Browser</button></a>
+          <a href="https://drive.google.com/file/d/1IWfyqQS1xVeOQIKEF1uXWiaJKkua_Ut9/view?usp=sharing" target="_blank" rel='noreferrer'><button className="mt-5 md:mt-0 p-3 rounded-lg bg-sky-950 text-white">View in Browser</button></a>
         </div>
         
         <div className='mb-5 xl:flex'>
