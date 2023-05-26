c780143cd8da555c37a6fecaa1ff3d13d4011765
kalen510 <kalen.wiley@gmail.com>
2023-05-25 21:24:06 -0400

    resume

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <main>
           <Skills />
           <Projects />
+          <Resume />
         </main>
       </div>
   );
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,19 +3,24 @@ import React from 'react';
 
 function Resume() {
   return (
-    <section>
-      <a href="https://drive.google.com/file/d/1IWfyqQS1xVeOQIKEF1uXWiaJKkua_Ut9/view?usp=sharing" target="_blank" rel='noreferrer'><button className="my-5 p-3 text-xl rounded-lg bg-sky-700 text-white">View in Browser</button></a>
-      <div className='mb-5 xl:flex'>
-        <img 
-          src={require('../../assets/images/Resume-1.png')}
-          alt='Resume page 1'
-          className='xl:w-1/2'
-        />
-        <img 
-          src={require('../../assets/images/Resume-2.png')}
-          alt='Resume page 2'
-          className='xl:w-1/2'
-        />
+    <section className="flex justify-center">
+      <div className="w-4/5">
+        <div id="section-title" className="text-5xl text-left pl-10 py-10">
+          Resume
+        </div>
+        <a href="https://drive.google.com/file/d/1IWfyqQS1xVeOQIKEF1uXWiaJKkua_Ut9/view?usp=sharing" target="_blank" rel='noreferrer'><button className="p-3 rounded-lg bg-sky-950 text-white">View in Browser</button></a>
+        <div className='mb-5 xl:flex'>
+          <img 
+            src={require('../../assets/images/Resume-1.png')}
+            alt='Resume page 1'
+            className='xl:w-1/2'
+          />
+          <img 
+            src={require('../../assets/images/Resume-2.png')}
+            alt='Resume page 2'
+            className='xl:w-1/2'
+          />
+        </div>
       </div>
     </section>
   );
